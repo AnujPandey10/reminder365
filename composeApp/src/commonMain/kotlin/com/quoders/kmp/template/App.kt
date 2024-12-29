410fd677f5f82d6dc257b419eb3c308a6aba671d
David Guerrero <david.guerrero@quoders.com>
2024-12-29 11:54:25 +0000

    Update App.kt

--- composeApp/src/commonMain/kotlin/com/quoders/kmp/template/App.kt
+++ composeApp/src/commonMain/kotlin/com/quoders/kmp/template/App.kt
@@ -4,9 +4,7 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
 import com.quoders.kmp.template.navigation.HomeScreenNavigation
 import org.jetbrains.compose.ui.tooling.preview.Preview
-import org.koin.core.annotation.KoinExperimentalAPI
 
-@OptIn(KoinExperimentalAPI::class)
 @Composable
 @Preview
 fun App() {
